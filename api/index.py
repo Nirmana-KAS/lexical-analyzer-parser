15a4fb2b99dae989da96c925aab199cca835b82e
Shehan Nirmana <135942979+Nirmana-KAS@users.noreply.github.com>
2025-10-15 03:07:29 +0530

    Add wsgi.py for PythonAnywhere
    Add wsgi.py for PythonAnywhere

--- api/index.py
+++ api/index.py
@@ -68,5 +68,5 @@ def health_check():
         'message': 'Lexical Analyzer & Parser API is running'
     })
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+#if __name__ == '__main__':
+#    app.run(debug=True, port=5000)
